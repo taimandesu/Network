a7a66177629f871305c3b8800ecf87fec477df29
yutohonda <43771570+yutohonda@users.noreply.github.com>
2019-06-28 03:34:11 +0900

    jsOauth追加
    jsだけでjson取得したぞ！！

---
+++ jsOauth2/simpleserver.bat
@@ -0,0 +1,4 @@
+@echo off
+ 
+起動するpython.exeまでのパスを記入("C:\Program Files\python\python.exe"など) %~dp0simpleserver.py
+pause
--- toilet3/js/search.js
+++ toilet3/js/search.js
@@ -1,6 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+var indexPage = fs.readFileSync('../index.html', 'utf8');
+
+var server = http.createServer(function(req, res){
+    var target = '';
+    switch(req.url){
+        case '/':
+        case '/index':
+            target = indexPage;
+            break;
+        default:
+            res.writeHead(404, {'ContentType': 'text/plain'});
+            res.end('not found');
+            return;
+    }   
+    
+    res.writeHead(200, {'ContentType': 'text/html'});
+    res.write(target);
+    res.end();
+});
+
+
 const twitter = require("twitter");
 //const module = require("twitter/lib/twitter.js");
-const fs = require("fs");
+//const fs = require("fs");
 
 const client = new twitter(JSON.parse(fs.readFileSync("secret.json","utf-8")));
 
@@ -14,10 +38,15 @@ client.get('search/tweets', params, function(error,tweets,response){
       console.log(tweets["statuses"][0]["text"]);
 
       //html書き換え
-      //document.getElementById('text').textContent = tweets["statuses"][0]["text"];
-      //document.getElementsByClassName('time').textContent = ["statuses"][0]["created_at"];
+      document.getElementById('text').textContent = tweets["statuses"][0]["text"];
+      document.getElementsByClassName('time').textContent = ["statuses"][0]["created_at"];
       fs.appendFileSync("search.json",JSON.stringify(tweets) + "\n","utf-8");
   }
 });
 
 //['members'][1]['powers'][2]
+
+
+server.listen(1234);
+console.log('サーバを起動しました');
+
